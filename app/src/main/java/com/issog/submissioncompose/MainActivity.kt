704255a8ae2abbc32606c0b73463494f7fc7c624
Rosyid Rosadi <rosyidrosadi15@gmail.com>
2025-06-10 06:35:33 +0700

    feat(home) : disable dark mode support

--- app/src/main/java/com/issog/submissioncompose/MainActivity.kt
+++ app/src/main/java/com/issog/submissioncompose/MainActivity.kt
@@ -11,7 +11,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SubmissionComposeTheme {
+            SubmissionComposeTheme(
+                darkTheme = false
+            ) {
                 BeritainApp()
             }
         }
