c42dc47eb1727ea0c27b07ff95fa5fa783593a68
Rosyid Rosadi <rosyidrosadi15@gmail.com>
2025-06-10 07:25:27 +0700

    chore() : remove testing api into app module

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -92,14 +92,4 @@ dependencies {
 
     // coil
     api(libs.coil.compose)
-
-    testImplementation(libs.junit)
-
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
